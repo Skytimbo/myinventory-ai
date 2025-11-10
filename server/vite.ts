e3bd1304ee660974cb41841c76c6618ba731e97f
Tim Scheffel <skytimbo@gmail.com>
2025-11-09 22:10:54 -0900

    fix: use explicit nanoid import path for Node.js ESM compat

--- server/vite.ts
+++ server/vite.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { createServer as createViteServer, createLogger } from "vite";
 import { type Server } from "http";
 import viteConfig from "../vite.config";
-import { nanoid } from "nanoid";
+import { nanoid } from "nanoid/index.js";
 
 const viteLogger = createLogger();
 
